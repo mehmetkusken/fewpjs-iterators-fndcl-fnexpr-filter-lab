f9e91f429e55703907e0f420f127fee40b744851
Mehmet <mehmetkusken@gmail.com>
2021-08-09 17:06:59 -0600

    commit tree e8ce6eafcfba3dd904a07f41a7c22a6535d55fe7

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution here
+d = ['Jose','matt','Sara','shannon']
+dO = [{name:'Jose'},
+{name:'matt'},
+{name:'Sara'},
+{name:'Shannon'},
+{name:'Mehmet'}
+]
+let filterDrivers = []
+function fuzzyMatch(driversArray, initialLetter)
+{
+    let filterDrivers = driversArray.filter(
+      driver => {
+        
+        return driver.toLowerCase().indexOf(initialLetter.toLowerCase()) === 0
+      }
+    )
+    return filterDrivers
+  }
+  function findMatching(driversArray, query) {
+    let search = driversArray.filter(
+      driver => {
+        return driver.toLowerCase() === query.toLowerCase()
+      }
+    )
+    return search
+  }
+  function matchName(driversArray, query) {
+    return driversArray.filter(
+      driverObj => driverObj.name === query
+    )
+  }
